b1baf009397d03d2e5a6bbec8663461718d38d17
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2024-04-29 02:05:59 +0300

    feat: implement home page

---
+++ components/home-card.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import Image from "next/image";
+import { cn } from "@/lib/utils";
+
+type HomeCardProps = {
+  className: string;
+  img: string;
+  title: string;
+  description: string;
+  handleClick: () => void;
+};
+
+const HomeCard = ({ className, img, title, description, handleClick }: HomeCardProps) => {
+  return (
+    <div
+      className={cn(
+        "px-4 py-6 flex flex-col justify-between w-full xl:max-w-[270px] min-h-[260px] rounded-[14px] cursor-pointer",
+        className
+      )}
+    >
+      <div className="flex-center glassmorphism size-12 rounded-[10px]" onClick={handleClick}>
+        <Image src={img} alt="meeting" width={27} height={27} />
+      </div>
+
+      <div className="flex flex-col gap-2">
+        <h1 className="text-2xl font-bold">{title}</h1>
+        <p className="text-lg font-normal">{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default HomeCard;
---
+++ components/meeting-type-list.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import React, { useState } from "react";
+import HomeCard from "./home-card";
+import { useRouter } from "next/navigation";
+
+type Props = {};
+
+const MeetingTypeList = (props: Props) => {
+  const router = useRouter();
+  const [meetingState, setMeetingState] = useState<
+    "isScheduleMeeting" | "isJoinMeeting" | "isInstantMeeting" | undefined
+  >();
+
+  return (
+    <section className="grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4">
+      <HomeCard
+        img="/icons/add-meeting.svg"
+        title="New Meeting"
+        description="Start an instant meeting"
+        handleClick={() => setMeetingState("isInstantMeeting")}
+        className="bg-orange-1"
+      />
+      <HomeCard
+        img="/icons/schedule.svg"
+        title="Schedule Meeting"
+        description="Plan your meeting"
+        handleClick={() => setMeetingState("isScheduleMeeting")}
+        className="bg-blue-1"
+      />
+      <HomeCard
+        img="/icons/recordings.svg"
+        title="View Recordings"
+        description="Check out your recordings"
+        handleClick={() => router.push("/recordings")}
+        className="bg-purple-1"
+      />
+      <HomeCard
+        img="/icons/join-meeting.svg"
+        title="Join Meeting"
+        description="Via invite link"
+        handleClick={() => setMeetingState("isJoinMeeting")}
+        className="bg-yellow-1"
+      />
+    </section>
+  );
+};
+
+export default MeetingTypeList;
