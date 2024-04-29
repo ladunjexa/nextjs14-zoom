061fd6b6187e390439b9047b1e523fad8ec7c4db
ladunjexa <110838700+ladunjexa@users.noreply.github.com>
2024-04-29 03:36:32 +0300

    feat: implement previous meetings page

--- app/(root)/(home)/previous/page.tsx
+++ app/(root)/(home)/previous/page.tsx
@@ -1,7 +1,11 @@
+import CallList from "@/components/call-list";
+
 export default function Previous() {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
       <h1 className="text-3xl font-bold">Previous</h1>
+
+      <CallList type="ended" />
     </section>
   );
 }
